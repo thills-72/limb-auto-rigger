0001368179b89f797aa7ded89edfbf7e8b9b3555
Tristan Hills <tristan.hills72@outlook.com>
2026-04-25 09:42:23 -0500

    Built and debugged a function to connect a UV coordinate point on the ribbon to a locator. This replaces what nHair would've been with a more stable and math based process.

--- ribbon-constructor.py
+++ ribbon-constructor.py
@@ -1,11 +1,42 @@
+"""
+takes a surface and a UV ratio position and pins a locator that follows the surface to that point, surface variable is a string and U and V are floats
+""" 
+def locator_to_surface_cv(surf, U, V):
+    #create point on surface node
+    infoNode = cmds.pointOnSurface(surf, parameterU = U, parameterV = V, constructionHistory = True)
+    #create four by four matrix and decompose matrix node
+    matrix = cmds.createNode("fourByFourMatrix")
+    decompose = cmds.createNode("decomposeMatrix")
+    #connect point on surface node vector information to four by four matrix inputs
+    cmds.connectAttr(f"{infoNode}.normalizedTangentUX", f"{matrix}.in00")
+    cmds.connectAttr(f"{infoNode}.normalizedTangentUY", f"{matrix}.in01")
+    cmds.connectAttr(f"{infoNode}.normalizedTangentUZ", f"{matrix}.in02")
+    cmds.setAttr(f"{matrix}.in03", 0)
+    cmds.connectAttr(f"{infoNode}.normalX", f"{matrix}.in10")
+    cmds.connectAttr(f"{infoNode}.normalY", f"{matrix}.in11")
+    cmds.connectAttr(f"{infoNode}.normalZ", f"{matrix}.in12")
+    cmds.setAttr(f"{matrix}.in13", 0)
+    cmds.connectAttr(f"{infoNode}.normalizedTangentVX", f"{matrix}.in20")
+    cmds.connectAttr(f"{infoNode}.normalizedTangentVY", f"{matrix}.in21")
+    cmds.connectAttr(f"{infoNode}.normalizedTangentVZ", f"{matrix}.in22")
+    cmds.setAttr(f"{matrix}.in23", 0)
+    cmds.connectAttr(f"{infoNode}.positionX", f"{matrix}.in30")
+    cmds.connectAttr(f"{infoNode}.positionY", f"{matrix}.in31")
+    cmds.connectAttr(f"{infoNode}.positionZ", f"{matrix}.in32")
+    cmds.setAttr(f"{matrix}.in33", 1)
+    #connect output of 4x4 matrix to decompose node
+    cmds.connectAttr(f"{matrix}.output", f"{decompose}.inputMatrix")
+    #connect output position and rotation of decompose to position and rotation of locator
+    locator = cmds.spaceLocator()[0]
+    cmds.connectAttr(f"{decompose}.outputTranslate", f"{locator}.translate")
+    cmds.connectAttr(f"{decompose}.outputRotate", f"{locator}.rotate")
+
+
 positions = []
 
 for jnt in cmds.ls(selection = True):
     position = cmds.xform(jnt, query = True, worldSpace = True, translation = True)
     positions.append(position)
-
-#crv1 = cmds.curve(degree = 1, point = positions)
-#crv2 = cmds.duplicate(crv)[0]
 
 """
 need to get perpendicular vector direction from the plane that the three joints are sitting within
@@ -61,21 +92,17 @@ nVectorNegA = [nVectorNeg[0] + A[0], nVectorNeg[1] + A[1], nVectorNeg[2] + A[2]]
 nVectorNegC = [nVectorNeg[0] + C[0], nVectorNeg[1] + C[1], nVectorNeg[2] + C[2]]
 negCurve = [nVectorNegA, nVectorNegB, nVectorNegC]
 
-"""
-cmds.xform(crv1 + ".cv[0]", worldSpace = True, translation = nVectorPosA)
-cmds.xform(crv1 + ".cv[1]", worldSpace = True, translation = nVectorPosB)
-cmds.xform(crv1 + ".cv[2]", worldSpace = True, translation = nVectorPosC)
-cmds.xform(crv2 + ".cv[0]", worldSpace = True, translation = nVectorNegA)
-cmds.xform(crv2 + ".cv[1]", worldSpace = True, translation = nVectorNegB)
-cmds.xform(crv2 + ".cv[2]", worldSpace = True, translation = nVectorNegC)
-"""
-
 crv1 = cmds.curve(degree = 1, point = [posCurve[0], posCurve[1]])
 crv2 = cmds.curve(degree = 1, point = [negCurve[0], negCurve[1]])
 crv3 = cmds.curve(degree = 1, point = [posCurve[1], posCurve[2]])
 crv4 = cmds.curve(degree = 1, point = [negCurve[1], negCurve[2]])
 
-surface1 = cmds.loft(crv1, crv2, constructionHistory = True, range = True, autoReverse = True)
-surface2 = cmds.loft(crv3, crv4, constructionHistory = True, range = True, autoReverse = True)
+surface1 = cmds.loft(crv1, crv2, constructionHistory = True, range = True, autoReverse = True)[0]
+surface2 = cmds.loft(crv3, crv4, constructionHistory = True, range = True, autoReverse = True)[0]
 
 cmds.delete(crv1, crv2, crv3, crv4)
+
+ribbon1 = cmds.rebuildSurface(surface1, rebuildType = 0, spansU = 1, spansV = 4, degreeU = 2, degreeV = 1)
+ribbon2 = cmds.rebuildSurface(surface2, rebuildType = 0, spansU = 1, spansV = 4, degreeU = 2, degreeV = 1)
+
+locator_to_surface_cv(ribbon1, 0.5, 0.5)
